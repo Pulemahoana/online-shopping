ff60588f6380a01e035210dec768e871837fb747
Pule David Mahoana <32096464+Pulemahoana@users.noreply.github.com>
2020-03-03 15:32:14 +0200

    Removing the Currency

--- src/Components/Order/Order.js
+++ src/Components/Order/Order.js
@@ -56,7 +56,7 @@ class ConnectedOrder extends Component {
             fontSize: 22
           }}
         >
-          Total price: {totalPrice} R
+          Total price: {totalPrice} $
         </div>
         <Button
           color="primary"
